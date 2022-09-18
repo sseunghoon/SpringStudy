459ad7737caf8a0b48db67a0857f932b88ad5967
sseunghoon <sseunghun99@naver.com>
2022-09-18 20:57:18 +0900

    connect h2 databae

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -16,6 +16,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository()
+        return new MemoryMemberRepository();
     }
 }
